28e627084c5997e97c772f46d149303d667569ac
MaryN-W <16004@coderacademy.edu.au>
2024-12-31 09:58:52 +1100

    Python intro practice done

---
+++ main.py
@@ -0,0 +1,63 @@
+
+# # print('Learn Python!')
+
+# # # Command l for comments
+# # print('Python is fun!')
+
+# # # Data types
+# # # int
+# # # float
+# # print(type(3.1459))
+# # # complex
+# # 2 + 3j
+# # print(type(2+3j))
+# # print(5 % 2.25) # Modulo (remainder)
+# # # Strings
+# # # "hello" 
+# # # or 
+# # # 'hello' # no distinction"
+# # print('It\'s Python') # Escaping
+
+# # # Concatenation or adding together
+# # print('python' + ' is fun') # + symbol is callled overloading, can add numbers or concatenate strings
+# # print('python'.lower()) #method - function which operates on certain data types, string in this case
+# # print('python'.upper())
+# # print('python'.replace('python', 'fun'))
+
+# # #Falsy values
+# # print(bool(0)) # false
+# # print(bool('')) # empty is false
+# # print(bool(None)) # when we need to represent the absence of a value
+# # print([]) # empty list
+# # print({}) # empty dict
+
+# # # comparison operators
+# # # <, >, <=, >=, ==
+
+# print(2 < 3)
+# print(4 <= 4)
+# print(2 == 2) # compare
+# # print(2 = 2) # assignment
+
+# # logical operators
+# # and, or, not
+
+# print(True and False)
+# print(False or True)
+# print(2 > 1 and 2 > 5)
+
+# Variables : area of memory that has a label attached to it
+# Declare a variable
+# num3 = 9
+# num6 = 10
+# print(num3 + num6)
+# num6 = 7
+# print(num3)
+# print(num6)
+
+# String interpolation: embedding variables or expressions directly into strings
+name = "Wangari"
+print('My name is ' + name + ' and I like Python')
+# f-string
+print(f'My name is {name} and I like Python') # simplifies above
+
